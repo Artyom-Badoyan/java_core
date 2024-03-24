18022988ac816105b96b93836b7a0626d2b68f3d
User <artyom.styopayevich@gmail.com>
2024-03-24 23:40:12 +0400

    сортировка

---
+++ src/comparableAndComparator/comparation/Test1.java
@@ -0,0 +1,18 @@
+package comparableAndComparator.comparation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Artyom");
+        list.add("Narine");
+        list.add("Karen");
+        System.out.println("перед сортировкой:" + list);
+        Collections.sort(list);
+        System.out.println("после сортировки:" + list);
+
+    }
+}
